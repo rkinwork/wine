705c36c349da6538b876cc5162f26f210b5cf494
rkinwork <rkinwork@gmail.com>
2020-10-24 10:42:52 +0300

    Add index.html generation from excel file

--- main.py
+++ main.py
@@ -1,5 +1,48 @@
+from datetime import date
+from pathlib import Path
+from collections import defaultdict
+
+import pandas as pd
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+WINERY_FOUNDATION_YEAR = 1920
+PRICE_LIST_FILE_PATH = Path('wine3.xlsx')
+PRICE_LIST_FILE_SHEET_NAME = 'Лист1'
+PRICE_LIST_HEADER_MAPPING = {'Название': 'name', 'Сорт': 'variety',
+                             'Цена': 'price', 'Картинка': 'image',
+                             'Категория': 'category', 'Акция': 'profitable'}
 
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+def main():
+    df = pd.read_excel(PRICE_LIST_FILE_PATH,
+                       sheet_name=PRICE_LIST_FILE_SHEET_NAME,
+                       engine='xlrd')
+    if set(df.columns.tolist()) != set(PRICE_LIST_HEADER_MAPPING):
+        print(f"There are no expected column names in {PRICE_LIST_FILE_PATH}")
+
+    df.rename(columns=PRICE_LIST_HEADER_MAPPING, inplace=True)
+
+    wines_by_category = defaultdict(list)
+    for category, group in df.groupby('category'):
+        wines_by_category[category] = group.fillna('').to_dict('records')
+
+    env = Environment(loader=FileSystemLoader('.'),
+                      autoescape=select_autoescape('html'))
+
+    template = env.get_template('template.html')
+
+    rendered_page = template.render(
+        winery_age=WINERY_FOUNDATION_YEAR - date.today().year,
+        wines_by_category=wines_by_category
+    )
+
+    with open('index.html', 'w', encoding='utf8') as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
